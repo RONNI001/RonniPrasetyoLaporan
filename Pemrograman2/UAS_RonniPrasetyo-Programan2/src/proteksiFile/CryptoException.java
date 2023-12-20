eecac7ddcdea580060d6a37a65243c9e09603f0e
RONNI001 <jajankang1@gmail.com>
2023-12-20 21:20:24 +0700

    first commit

---
+++ Pemrograman2/UAS_RonniPrasetyo-Programan2/src/proteksiFile/CryptoException.java
@@ -0,0 +1,15 @@
+package proteksiFile;
+
+/**
+ *
+ * @author ASUS
+ */
+public class CryptoException extends Exception {
+
+    public CryptoException() {
+    }
+
+    public CryptoException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
